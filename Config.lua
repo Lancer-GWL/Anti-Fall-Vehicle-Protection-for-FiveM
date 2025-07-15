5258c296dc94c4ae2a61a937b1366484ab889e51
JustMe <147296669+Lancer-GWL@users.noreply.github.com>
2025-07-15 23:31:06 +0530

    Config.lua

---
+++ Config.lua
@@ -0,0 +1,7 @@
+Config = {}
+
+Config.FallOffSpeed = 250 -- KM/H threshold to allow falling
+
+Config.NotifyDuration = 3000 -- milliseconds for on-screen messages
+
+Config.NotifySound = true -- Play sound on notifications (including fall)
