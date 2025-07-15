a79f5bc3c5d09c8f0e079e7218b5445643c1c421
JustMe <147296669+Lancer-GWL@users.noreply.github.com>
2025-07-15 23:34:14 +0530

    fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Lancer'
+description 'Anti-fall off vehicles script with commands and on-screen notifications'
+version '1.0.0'
+
+client_scripts {
+    'config.lua',
+    'client.lua'
+}
